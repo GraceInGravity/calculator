9a92fbbd3889b7fcf1b32a3f37f32e8c5bb7d311
Isaac Hall and Nuala Gray <nuala@>
2019-01-14 14:40:40 -0800

    This adds the multiply, divide, and subtract functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,66 @@
-var add = function(number1, number2){
-	return number1 + number2;
+alert("What's wrong?!");
 
+var add = function(number1, number2) {
+  return number1 + number2;
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter a number2:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
 
-alert(add(number1, number2));
+
+
+
+
+
+
+
+
+
+alert("What's wrong2?!");
+
+// var
+add = function(number1, number2){
+	return number1 + number2;
+};
+
+alert("What's wrong3?!");
+
+// var
+var number1 = parseInt(prompt("Enter a number1:"));
+// var
+var number2 = parseInt(prompt("Enter a number2:"));
+//
+alert("Your numbers added = " + add(number1, number2));
+//
+var subtract = function(number1, number2){
+	return number1 - number2;
+
+};
+//
+// // var number1 = parseInt(prompt("Enter a number:"));
+// // var number2 = parseInt(prompt("Enter a number2:"));
+//
+alert("Your numbers subtracted = " + subtract(number1, number2));
+//
+var multiply = function(number1, number2){
+	return number1 * number2;
+
+};
+//
+// // var number1 = parseInt(prompt("Enter a number:"));
+// // var number2 = parseInt(prompt("Enter a number2:"));
+//
+//
+alert("Your numbers multiplied = " + multiply(number1, number2));
+//
+var divide = function(number1, number2){
+	return number1 / number2;
+
+};
+//
+// // var number1 = parseInt(prompt("Enter a number:"));
+// // var number2 = parseInt(prompt("Enter a number2:"));
+//
+alert("Your numbers divided = " + divide(number1, number2));
